081b95b4d8f4e352e925a6ab4e9c2086fdeb9293
Abhijit Dey <87671060+Avichiii@users.noreply.github.com>
2022-12-22 09:43:06 +0530

    Add files via upload

---
+++ notvirus.py
@@ -0,0 +1,22 @@
+#input form the user
+virusText = input("Enter a String :) : ")
+
+#this function will create a file
+with open(virusText, 'w') as f:
+    file = f.write("this is a test file")
+
+
+#loops thorugh
+
+for i in range(1,10):
+#this is creating a new file
+    a = open(virusText, "a")
+    b = a.write("\n this will append to the output file ")
+    a.close() 
+    # print(i)
+
+#this is reading from the file
+f = open(virusText, "r")
+d = f.read()
+print(d,end="")
+f.close()
