323d91fa7cb3102233c8de973b7df452a4aa59f6
Abhijit Dey <87671060+Avichiii@users.noreply.github.com>
2022-12-23 00:25:51 +0530

    Update notvirus.py

--- notvirus.py
+++ notvirus.py
@@ -2,7 +2,7 @@
 virusText = input("Enter a String :) : ")
 
 #this function will create a file
-with open(virusText, 'w') as f:
+with open(virusText + ".txt", 'w') as f:
     file = f.write("this is a test file")
 
 
@@ -10,13 +10,13 @@ with open(virusText, 'w') as f:
 
 for i in range(1,10000000000000000000000000000000000000000000000000000):
 #this is creating a new file
-    a = open(virusText, "a")
+    a = open(virusText + ".txt", "a")
     b = a.write("\n this will append to the output file ")
     a.close() 
     # print(i)
 
 #this is reading from the file
-f = open(virusText, "r")
+f = open(virusText + ".txt", "r")
 d = f.read()
 print(d,end="")
 f.close()
